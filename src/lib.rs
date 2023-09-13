dd5bbd7966b6dfc85929aaf6f970f87fe60cafe6
jbiccler <jaron.biccler@gmail.com>
2023-09-13 10:01:30 +0200

    feat: recursive and non-recursive bubble sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,76 @@
+#![feature(test)]
+extern crate test;
 pub mod sort;
 
 #[cfg(test)]
 mod tests {
     use crate::sort::bubble_sort;
+    use crate::sort::bubble_sort_recursive;
+    use test::Bencher;
 
-    #[test]
-    fn bubble_int() {
-        let mut v = vec![4, 3, 1, 3, 5, 6];
-        bubble_sort(&mut v);
-        assert_eq!(v, vec![1, 3, 3, 4, 5, 6]);
+    #[bench]
+    fn bubble_int(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v = vec![4, 3, 1, 3, 5, 6];
+            bubble_sort(&mut v);
+            assert_eq!(v, vec![1, 3, 3, 4, 5, 6]);
+        })
     }
-    #[test]
-    fn bubble_float() {
-        let mut v = vec![4., 3., 1., 3., 5., 6.];
-        bubble_sort(&mut v);
-        assert_eq!(v, vec![1., 3., 3., 4., 5., 6.]);
+    #[bench]
+    fn bubble_float(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v = vec![4., 3., 1., 3., 5., 6.];
+            bubble_sort(&mut v);
+            assert_eq!(v, vec![1., 3., 3., 4., 5., 6.]);
+        })
     }
-    #[test]
-    fn bubble_unit() {
-        let mut v: Vec<i32> = vec![];
-        bubble_sort(&mut v);
-        assert_eq!(v, Vec::<i32>::new());
+    #[bench]
+    fn bubble_unit(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v: Vec<i32> = vec![];
+            bubble_sort(&mut v);
+            assert_eq!(v, Vec::<i32>::new());
+        })
     }
-    #[test]
-    fn bubble_single() {
-        let mut v: Vec<i32> = vec![1];
-        bubble_sort(&mut v);
-        assert_eq!(v, Vec::<i32>::from([1]));
+    #[bench]
+    fn bubble_single(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v: Vec<i32> = vec![1];
+            bubble_sort(&mut v);
+            assert_eq!(v, Vec::<i32>::from([1]));
+        })
+    }
+
+    #[bench]
+    fn bubble_int_recursive(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v = vec![4, 3, 1, 3, 5, 6];
+            bubble_sort_recursive(&mut v);
+            assert_eq!(v, vec![1, 3, 3, 4, 5, 6]);
+        })
+    }
+    #[bench]
+    fn bubble_float_recursive(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v = vec![4., 3., 1., 3., 5., 6.];
+            bubble_sort_recursive(&mut v);
+            assert_eq!(v, vec![1., 3., 3., 4., 5., 6.]);
+        })
+    }
+    #[bench]
+    fn bubble_unit_recursive(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v: Vec<i32> = vec![];
+            bubble_sort_recursive(&mut v);
+            assert_eq!(v, Vec::<i32>::new());
+        })
+    }
+    #[bench]
+    fn bubble_single_recursive(b: &mut Bencher) {
+        b.iter(|| {
+            let mut v: Vec<i32> = vec![1];
+            bubble_sort_recursive(&mut v);
+            assert_eq!(v, Vec::<i32>::from([1]));
+        })
     }
 }
--- src/sort.rs
+++ src/sort.rs
@@ -3,6 +3,24 @@ pub fn bubble_sort<T: PartialOrd>(v: &mut Vec<T>) {
     if v.is_empty() {
         return;
     }
+    loop {
+        let mut swapped: bool = false;
+        for i in 0..(v.len() - 1) {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        };
+    }
+}
+pub fn bubble_sort_recursive<T: PartialOrd>(v: &mut Vec<T>) {
+    // simple in place bubble sort but recursive
+    if v.is_empty() {
+        return;
+    }
     let mut swapped: bool = false;
     for i in 0..(v.len() - 1) {
         if v[i] > v[i + 1] {
